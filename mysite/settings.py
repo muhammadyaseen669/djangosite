a8deafbf8fd8bc5a9209441c16f5d65097cf4a27
muhammad yaseen <muhammady669@gmail.com>
2020-12-01 15:03:38 +0500

    final commit 3:03 pm 1-dec-202

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3w5+j6wh0^w9pjv14egctmtb^(7cq#whl%q(_8t*_(q2y^5z@a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
